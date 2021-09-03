d2ebfc14bfeac258ee46f69abe057e5536b39a64
Jenny Lin <misslinyang91@gmail.com>
2021-09-03 10:12:30 +1000

    fetched the jokes

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // https://icanhazdadjoke.com/api
+const jokeBtn = document.querySelector("button");
+const jokePara = document.querySelector(".joke");
+
+jokeBtn.addEventListener("click", getJoke);
+
+async function getJoke() {
+  const jokeURL = "https://icanhazdadjoke.com/";
+  const response = await fetch(jokeURL, {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  const responseData = await response.json();
+
+  console.log(responseData.joke);
+  jokePara.innerHTML = responseData.joke;
+}
